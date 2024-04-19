4cd76924ff8e7883601a40fa25ed9b51799e4b9f
Frans Imanuel <fransimanuel99@gmail.com>
2024-04-19 16:28:41 +0700

    w

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting---")
 	t, err := tail.TailFile("/var/log/apache2/access.log", tail.Config{Follow: true, ReOpen: true})
 	if err != nil {
 		fmt.Println(err)
+		panic(1)
 	}
 	for line := range t.Lines {
 		fmt.Println(line.Text)
 	}
+	fmt.Println("Finished---")
 }
